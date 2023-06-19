018d5ebd0e7af37573034d049e67122aff1cccd3
fitriadl <201240000993@unisnu.ac.id>
2023-06-20 00:39:09 +0700

    Home Screen UI

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import '../theme/theme.dart';
-import 'constants.dart';
+import 'package:twitter_clone/theme/pallete.dart';
+import 'package:twitter_clone/constants/constants.dart';
 
 class UIConstants {
   static AppBar appBar() {
@@ -13,4 +13,10 @@ class UIConstants {
       centerTitle: true,
     );
   }
+
+  static List<Widget> bottomTabBarPages = [
+    Text('Feed Screen'),
+    Text('Search Screen'),
+    Text('Notification Screen'),
+  ];
 }
